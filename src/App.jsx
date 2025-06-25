8ccf43346bd63987d9bb206eb2210aff393b1c5f
TechTirth <t1eerth.v@gmail.com>
2025-06-25 22:32:35 +0530

    Refactor index.html title and add initial App component with card fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cards")
+      .then(res => res.json())
+      .then(data => setCards(data));
+  }, []);
+
   return (
-    <>
-      
-    </>
-  )
+    <div className="p-4 flex flex-wrap gap-4">
+      {cards.map(card => (
+        <div key={card.id} className="bg-white shadow rounded p-4 w-64">
+          <h2 className="font-bold text-xl">{card.title}</h2>
+          <p>{card.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default App
